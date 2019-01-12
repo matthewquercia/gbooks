457b90017a0c8ab996173587dd6cac40b1f99bac
matthew <quercia.matt@gmail.com>
2019-01-11 19:45:25 -0500

    Updates

---
+++ precache-manifest.bfa56e7eb7d78c14b279f1a150543ea4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "488f1b10c9d5a95bde4d35e25225aff8",
+    "url": "/gbooks/static/media/books.488f1b10.jpeg"
+  },
+  {
+    "revision": "87d34fe4af6d3f030b76",
+    "url": "/gbooks/static/js/runtime~main.87d34fe4.js"
+  },
+  {
+    "revision": "258ed2f0891048b2dfb8",
+    "url": "/gbooks/static/js/main.258ed2f0.chunk.js"
+  },
+  {
+    "revision": "227d73aca94a60502d30",
+    "url": "/gbooks/static/js/1.227d73ac.chunk.js"
+  },
+  {
+    "revision": "258ed2f0891048b2dfb8",
+    "url": "/gbooks/static/css/main.a7d77cd9.chunk.css"
+  },
+  {
+    "revision": "05905e86fa89cc4f31d818f312230db6",
+    "url": "/gbooks/index.html"
+  }
+];
